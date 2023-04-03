6dbd77df4c4e851330ba263c61dff20555eba7e5
Seongtae <stae1102@gmail.com>
2023-04-03 22:56:05 +0900

    feat: 자바스크립트 동시적인 처리(동시성) 추가

---
+++ Example 1/reader.js
@@ -0,0 +1,14 @@
+import fs from 'fs/promises';
+
+const getNum = async (filename) => {
+  return parseInt(await fs.readFile(filename, 'utf8'), 10);
+};
+
+try {
+  const numberPromises = [1, 2, 3].map((i) => getNum(`${i}.txt`));
+  const numbers = await Promise.all(numberPromises);
+  console.log(numbers[0] + numbers[1] + numbers[2]);
+} catch (err) {
+  console.error('Something went wrong:');
+  console.error(err);
+}
